0997fbf8b716771db290423f0d57413241943fb0
Anton Kanashuk <hlamjoo@gmail.com>
2017-09-27 17:10:22 +0300

    Calculator

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,22 @@
 public class Calculate {
-    public static void main(String[] atg) {
+    public static void main(String[] arg) {
         System.out.println("Hello world");
+        int first = Integer.valueOf(arg[1]);
+        int second = Integer.valueOf(arg[2]);    
+        
+        switch(arg[0]){
+            case "+":
+                System.out.println(first + second);
+                break;
+            case "-":
+                System.out.println(first - second);
+                break;
+            case "m":
+                System.out.println(first * second);
+                break;
+            case "d":
+                System.out.println(first / second);
+                break;
+        }
     }
 }
