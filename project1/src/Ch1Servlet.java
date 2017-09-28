1eb18ce7693f0ebb3c82e9dbb1172d7e09c50409
Anton Kanashuk <hlamjoo@gmail.com>
2017-09-28 18:08:28 +0300

    Head First

---
+++ project1/src/Ch1Servlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Ch1Servlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        java.util.Date today = new java.util.Date();
+        out.println("<html>" +
+                    "<body>" +
+                    "<hi align=center>HF\'s serv</h1>" +
+                    "<br1>" + today + "</body>" + "</html>");
+    }
+}
